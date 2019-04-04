1f7de36ede3973611e1d9c6fd53e0474200887df
Carl Danley <carldanley@gmail.com>
2019-04-04 01:25:45 -0400

    Removed unnecessary validator package

--- src/routes/v1/signup/signup.go
+++ src/routes/v1/signup/signup.go
@@ -3,13 +3,9 @@ package signup
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/kubesmith/kubesmith-server/src/server"
-	"gopkg.in/go-playground/validator.v9"
 )
 
-var validate *validator.Validate
-
 func RegisterRoutes(group *gin.RouterGroup, server *server.Server) {
-	validate = validator.New()
 	signup := group.Group("/signup")
 	signup.POST("/", server.WrapHandler(SignupPost))
 }
